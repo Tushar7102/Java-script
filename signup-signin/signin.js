64cb95be3cf28f9f34da981a3109207b2656c188
Tushar7102 <tp126090@gmail.com>
2024-04-16 12:11:19 +0530

    signin-project

---
+++ signup-signin/signin.js
@@ -0,0 +1,34 @@
+const signupform = document.getElementById("signup");
+const signinform = document.getElementById("signin");
+signupform.addEventListener("submit", (e) => {
+  e.preventDefault();
+  let signupobj = {
+    name: document.querySelector("#name").value,
+    email: document.querySelector("#email").value,
+    country: document.querySelector("#country").value,
+    phone: document.querySelector("#phone").value,
+    password: document.querySelector("#password").value,
+  };
+  // console.log(signupobj);
+  localStorage.setItem("Profile", JSON.stringify(signupobj));
+});
+
+signinform.addEventListener("submit", (e) => {
+  e.preventDefault();
+  let signinobj = {
+    name: document.querySelector("#signin-name").value,
+    email: document.querySelector("#signin-email").value,
+    password: document.querySelector("#signin-password").value,
+  };
+  //    console.log(signinobj);
+  const loginformdata = JSON.parse(localStorage.getItem("Profile"));
+  // console.log(loginformdata)
+  if (
+    signinobj.email == loginformdata.email &&
+    signinobj.password == loginformdata.password
+  ) {
+    alert("Login Succesfully");
+  } else {
+    alert("invaild  username or password");
+  }
+});
